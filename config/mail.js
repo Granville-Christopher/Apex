9d630daa92fd079b67a97dcae8dbbc91845e48a3
chucky-golden <chukschibyke92@gmail.com>
2025-06-26 06:28:04 +0100

    mail

--- config/mail.js
+++ config/mail.js
@@ -4,7 +4,8 @@ const sendOtpEmail = async (to, otp) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false,
+    // secure: false,
+    secure: process.env.EMAIL_PORT == 465,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
