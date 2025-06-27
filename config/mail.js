851ba1d999ce4e1e6cc89f3b6c4f6b35bc7feba1
Granville Christopher <christophergranville2@gmail.com>
2025-06-27 16:06:36 -0700

    latest commit

--- config/mail.js
+++ config/mail.js
@@ -2,10 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendOtpEmail = async (to, otp) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    // secure: false,
-    secure: process.env.EMAIL_PORT == 465,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
